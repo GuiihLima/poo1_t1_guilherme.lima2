a70dba8e7f4520a3d8efe6a3c86db988ba128582
guiih.lima <35679330+By-Henrique@users.noreply.github.com>
2021-12-06 19:20:42 -0300

    Incomplete

---
+++ LAB01 - Procedural/ex01.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef struct
+{
+    int codigo;
+    float preco;
+    string nome, fabricante;
+} prod;
+
+int cad_prod()
+{
+    prod init;
+    int codigo = 1;
+    float preco = 1.11;
+    string nome = "bolacha";
+    string fabricante = "mabel";
+
+    cout << 
+}
+
+int main()
+{
+    cad_prod();
+}
